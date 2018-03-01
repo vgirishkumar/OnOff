67345ff4233f98784550fa6c1524dea41bd58dec
RKUMAR27 <RKUMAR27@IND6V7XHM2.vistcorp.ad.visteon.com>
2018-03-01 18:49:04 +0530

    Customize the Custom Perspective

--- components/app/ui/com.visteon.onoff.dev.app/src/com/visteon/onoff/dev/app/Activator.java
+++ components/app/ui/com.visteon.onoff.dev.app/src/com/visteon/onoff/dev/app/Activator.java
@@ -10,7 +10,7 @@ import org.osgi.framework.BundleContext;
 public class Activator extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.eclipse.visteon.dev.product"; //$NON-NLS-1$
+	public static final String PLUGIN_ID = "com.visteon.onoff.dev.app"; //$NON-NLS-1$
 
 	// The shared instance
 	private static Activator plugin;
